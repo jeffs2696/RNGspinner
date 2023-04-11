dc06179338685ac46c125db2c3f9cc5aa6dfcc32
victor-schumann <schumann@tutamail.com>
2023-04-11 22:45:35 +0100

    Edited probs and added date/time to rewards.txt

--- RNGspinner/spinner.py
+++ RNGspinner/spinner.py
@@ -1,27 +1,39 @@
-import random 
+import random
+import datetime
 
-reward_probs = { 
-        'Steam Deck': 0.00001,
-        'nothing': 0.5,
-        'next shower is warm': 0.05, 
-        '10 mins social media': 0.1, 
-        'game of league': 0.05, 
-        'eat takeout': 0.05, 
-        '10 mins of anything': 0.05, 
-        '10 mins of chess': 0.05, 
-        'read an article': 0.1, 
-        'two spins': 0.04999, 
+reward_probs = {
+    'Steam Deck': 0.005,
+    'Two spins': 4.995,
+    'Next shower is warm': 5,
+    'Game of https://chess.com': 5,
+    'Eat fancy tonight': 5,
+    'Anything for 15 minutes': 5,
+    'Read a book for 15 minutes': 5,
+    'Browse Reddit for 15 minutes': 5,
+    'Read an article from https://thehackernews.com/': 5,
+    'Read an article from https://medium.com/': 5,
+    'Read an article from https://www.sciencealert.com/': 5,
+    'Nothing': 50,
 }
-def spin(): 
-    return random.choices(list (reward_probs. keys ()), list (reward_probs. values () )) [0]
-def main(): 
 
-    spin_result = spin() 
+
+def spin():
+    rewards = list(reward_probs.keys())
+    probabilities = list(reward_probs.values())
+
+    return random.choices(rewards, probabilities)[0]
+
+
+def main():
+    spin_result = spin()
     print('Your Random Reward is...')
-    print(spin_result) 
-    if (spin_result != 'nothing'): 
-        with open('rewards.txt', 'a') as f: 
-            f.write(spin_result +'\n') 
-if __name__ == '__main__': main()
+    print(spin_result)
+
+    if spin_result != 'Nothing':
+        date_time = datetime.datetime.now().strftime("%H:%M, %d/%m/%Y")
+        with open('rewards.txt', 'a') as f:
+            f.write(spin_result + '\n' + date_time + '\n\n')
 
 
+if __name__ == '__main__':
+    main()
